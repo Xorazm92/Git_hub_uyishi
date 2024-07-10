b8a134bc156557c3c8115f2a3377ebdd6daf621b
Xorazm92 <rayadunia2021@gmail.com>
2024-07-10 23:31:16 +0500

    ishlagaani

--- v1.py
+++ v1.py
@@ -1,10 +1,14 @@
-n =int(input("Nechtalik list kiritasiz:  "))
-lst1 =[]
+n = int(input("Nechtalik ro'yxat kiritasiz: "))
+lst1 = []
 
 for i in range(n):
     lst1.append(int(input(f"{i+1}-chi elementni kiriting: ")))
+
 lst2 = []
-x =len(lst1)
-for i in range(1,x):
-    lst2.append(lst1[i]-lst1[i-1])
-print(max (lst2))
+
+for i in range(len(lst1)-1):
+    if lst1[i] * lst1[i+1] > 0: 
+        lst2.append(f"{lst1[i]} {lst1[i+1]}")
+     
+
+print("lst2 ro'yxati:", lst2)
